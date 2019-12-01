947d5246d2d57735b2eae37e32fb688e24fcb577
satyam <titi.satyam@gmail.com>
2019-12-01 17:53:10 +0530

    first commit

---
+++ virajetech_ios/virajetech_ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  virajetech_ios
+//
+//  Created by SATYAM on 01/12/19.
+//  Copyright © 2019 SATYAM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
